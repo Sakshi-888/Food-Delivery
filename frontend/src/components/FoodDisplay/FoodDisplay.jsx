39518ed5659815e9b286d0fc78d8ff23013bdabd
Sakshi Sharma <sharmasakshi8546@gmail.com>
2024-05-22 21:43:23 +0530

    hello

---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,16 @@
+import React,{useContext} from 'react'
+import './FoodDisplay.css'
+
+
+const FoodDisplay = ({category}) => {
+
+const {food_list} = useContext(StoreContext)
+
+  return (
+    <div className='food-display' id='food-display'>
+      <h2>Top dishes near you</h2>
+    </div>
+  )
+}
+
+export default FoodDisplay
