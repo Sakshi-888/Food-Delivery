17dcc0928aef76b278482c9f61477d736a843fe9
Sakshi Sharma <sharmasakshi8546@gmail.com>
2024-07-12 16:14:08 +0530

    Completed Finally!

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 
 const App = () => {
 
-const url="http://localhost:4000"
+const url="https://food-delivery-backend-mylk.onrender.com"
 
   return (
     <div>
